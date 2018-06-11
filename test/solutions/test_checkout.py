281f1244291570939742d48016902fc42acc0996
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 09:39:37 +0000

    Mon Jun 11 09:39:37 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -3,35 +3,35 @@ from lib.solutions.checkout import checkout
 
 
 class TestCheckout(unittest.TestCase):
-#    def test_checkout(self):
-#        self.assertEqual(checkout("AB"), 80)
-#
-#    def test_checkout2(self):
-#        self.assertEqual(checkout("AAAAA"), 200)
-#
-#    def test_checkout_dirty_input(self):
-#        self.assertEqual(checkout(" A   B "), -1)
-#
-#    def test_checkout_multi(self):
-#        self.assertEqual(checkout("ABA"), 130)
-#
-#    def test_checkout_offer(self):
-#        self.assertEqual(checkout("AABBA"), 175)
-#
-#    def test_checkout_illegal(self):
-#        self.assertEqual(checkout("pandas"), -1)
-#
-#    def test_checkout_illegal2(self):
-#        self.assertEqual(checkout("AX"), -1)
-#
-#    def test_checkout_new_product(self):
-#        self.assertEqual(checkout("EE"), 80)
-#
-#    def test_checkout_bogof(self):
-#        self.assertEqual(checkout("BEE"), 80)
-#
-#    def test_checkout_bogof2(self):
-#        self.assertEqual(checkout("EEB"), 80)
+    def test_checkout(self):
+        self.assertEqual(checkout("AB"), 80)
+
+    def test_checkout2(self):
+        self.assertEqual(checkout("AAAAA"), 200)
+
+    def test_checkout_dirty_input(self):
+        self.assertEqual(checkout(" A   B "), -1)
+
+    def test_checkout_multi(self):
+        self.assertEqual(checkout("ABA"), 130)
+
+    def test_checkout_offer(self):
+        self.assertEqual(checkout("AABBA"), 175)
+
+    def test_checkout_illegal(self):
+        self.assertEqual(checkout("pandas"), -1)
+
+    def test_checkout_illegal2(self):
+        self.assertEqual(checkout("AX"), -1)
+
+    def test_checkout_new_product(self):
+        self.assertEqual(checkout("EE"), 80)
+
+    def test_checkout_bogof(self):
+        self.assertEqual(checkout("BEE"), 80)
+
+    def test_checkout_bogof2(self):
+        self.assertEqual(checkout("EEB"), 80)
 
     def test_checkout_bogof_first(self):
         self.assertEqual(checkout("EEEEBB"), 160)
