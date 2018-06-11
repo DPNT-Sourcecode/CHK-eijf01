1e843cfa93147b6e610ed8c2bb62d0450c330084
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 07:21:05 +0000

    Mon Jun 11 07:21:05 UTC 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,15 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+
 def sum(x, y):
-    raise NotImplementedError()
+    if isinstance(x, str):
+        if x.isnumeric(x):
+            x = int(x)
+        else:
+            raise TypeError("first input must be an integer")
+    
+        
+
+    if (not isinstance(x, int)
+    return 
+
