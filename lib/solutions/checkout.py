a5c8155d427cf5cd506399e8daddd05ec6904f6c
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 08:44:53 +0000

    Mon Jun 11 08:44:53 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -15,20 +15,21 @@ def checkout(skus):
   if isdirty(skus):
       return -1
 
-  offers = { 'A': [(3, 139)], 'B': [(2, 45)]}
+  offers = { 'A': [(3, 130)], 'B': [(2, 45)]}
   costs = { 'A': 50, 'B': 30, 'C': 20, 'D': 15 }
   a_skus = sorted(list(skus))
 
   val = 0
   for k, g in groupby(a_skus):  # identity lambda as def
       count = len(list(g))
-      print("considering %r, count %d" % (k, count))
+      #print("considering %r, count %d" % (k, count))
       # consider offers first
       if k in offers:
           # hmm, possible multiple offers.. order.. best val..
           for quantity, cost in offers[k]:
-              print("considering offer on %r, %d for %d" % (k, quantity, cost))
-              while count > quantity:
+              #print("considering offer on %r, %d for %d" % (k, quantity, cost))
+              while count >= quantity:
+                  #print("offer requirements met, %d for %d on %r" % (quantity, cost, k))
                   val += cost
                   count -= quantity
       # single item cost
--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -6,18 +6,18 @@ class TestSum(unittest.TestCase):
     def test_checkout(self):
         self.assertEqual(checkout("AB"), 80)
 
-    def test_checkout_clean_input(self):
-        self.assertEqual(checkout(" A   B "), 80)
+    def test_checkout_dirty_input(self):
+        self.assertEqual(checkout(" A   B "), -1)
 
     def test_checkout_multi(self):
-        self.assertEqual(checkout("A B A"), 130)
+        self.assertEqual(checkout("ABA"), 130)
 
     def test_checkout_offer(self):
-        self.assertEqual(checkout("A A B B A"), 175)
+        self.assertEqual(checkout("AABBA"), 175)
 
     def test_checkout_illegal(self):
         self.assertEqual(checkout("pandas"), -1)
 
     def test_checkout_illegal2(self):
-        self.assertEqual(checkout("A E"), -1)
+        self.assertEqual(checkout("AE"), -1)
 
