c147a2e899713cab3e0fcd6d82bcdb434cb183d5
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 08:30:00 +0000

    Mon Jun 11 08:30:00 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,7 +5,7 @@ def isdirty(s):
    return True if re.search("[^A-D]") else False;
 
 def clean(s):
-   return 
+   return re.sub("[^A-D]", "")
 
 
 # noinspection PyUnusedLocal
@@ -13,11 +13,19 @@ def clean(s):
 def checkout(skus):
 
   if isdirty(skus):
+      
+  offers = { 'A': [(3, 139)], 'B': [(2, 45)]}
+  costs = { 'A': 50, 'B': 30, 'C': 20, 'D': 15 }
+  a_skus = sorted(skus.split(''))
 
+  val = 0
+  for k, g in a_skus.groupby(a_skus)  # identity lambda as def
+      count = len(list(g))
+      # consider offers first
+      if k in offers
+          # hmm, possible multiple offers.. order.. best val..
+          for o in p_offers = of
+          while count > 
 
-  costs = { 'AAA': 130,  'A': 50, 'B': 30, 'C': 20, 'D': 15
-  skus = 
-
-  counts = 
-
+      val += (count * costs[k])
 
