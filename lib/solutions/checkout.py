4df362253a8e863aa75b0c4868df42a48cb94ee9
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 08:18:00 +0000

    Mon Jun 11 08:18:00 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,23 @@
+import itertools
+import re
+
+def isdirty(s):
+   return True if re.search("[^A-D]") else False;
+
+def clean(s):
+   return 
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+  if isdirty(skus):
+
+
+  costs = { 'AAA': 130,  'A': 50, 'B': 30, 'C': 20, 'D': 15
+  skus = 
+
+  counts = 
+
+
